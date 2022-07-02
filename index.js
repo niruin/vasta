a618299a10c08515e0c3af5998b6aad8e1449e1b
qq <nftless@gmail.com>
2022-07-02 16:29:59 +0300

    add Procfile

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(express.json())
 app.use('/api', authRouter)
 
 app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 const start = async () => {
