c8ac5ce6adbc99434675fdc3a9e8769018f04ef0
qq <nftless@gmail.com>
2022-07-16 15:06:21 +0300

    edit config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   ACCESS_TOKEN_SECRET: 'something_token_secret_key',
   REFRESH_TOKEN_SECRET: 'something_refresh_token_secret_key',
   // mongoDB: 'mongodb+srv://admin:9Rym5qbuaPuSAmz@cluster0.wh0as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  mongoDB: 'mongodb+srv://admin2:ehQs0IhYcm6f4Zac@cluster0.wh0as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  mongoDB: 'mongodb+srv://admin2:Q4AwNBbFX7egxJjb@cluster0.wh0as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
 //password was changed
 
